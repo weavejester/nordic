0a273e21653b69eea3ca5986b96a3f38941cf426
James Reeves <jreeves@weavejester.com>
2016-01-28 18:31:44 +0000

    Add Brutha dependency to example project

--- example/project.clj
+++ example/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
+                 [brutha "0.2.0"]
                  [nordic "0.1.0-SNAPSHOT"]])
